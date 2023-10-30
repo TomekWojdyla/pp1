c0a199ee489097eaf730f712a1872faf8344c077
TomekWojdyla <tw.kota@gmail.com>
2023-10-30 08:41:02 +0100

    zadania 8-10

---
+++ 02-TypesAndVariables/02_TW/task10.py
@@ -0,0 +1,5 @@
+name = "Tomek"
+age = 40
+height = 176
+age_in_6 = age+6
+print(f"My name is {name}, I am {age} years old and my height is {height}. In 6 years i will be {age_in_6} years old")
---
+++ 02-TypesAndVariables/02_TW/task9.py
@@ -0,0 +1,12 @@
+n1 = 15
+n2 = 1
+n3 = 8
+n4 = 6
+n5 = 31
+sum = n1 + n2 + n3 + n4 + n5
+print("Sum of variables equals: ",sum)
+Sum_of_sqrt = n1**2 + n2**2 + n3**2 + n4**2 + n5**2
+print("Sum of squared variables  equals: ", Sum_of_sqrt) 
+quotient_3_5 = n3//n5
+print("Quotient of variable 3 and 5 equals: ", quotient_3_5)
+print("Validate if variable 3 equals variable 4:",3==4)
